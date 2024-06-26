4c40c92c3c7e512c777144207696ac303885c7f6
gerome about <gerome11@outlook.com>
2024-06-26 14:48:20 +0800

    Initial

---
+++ Migrations/20240626021649_Initial.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace inspirited.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Characters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Type = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Details = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Characters", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    PlayerName = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CharacterId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Statistics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CharacterId = table.Column<int>(type: "int", nullable: false),
+                    Strength = table.Column<int>(type: "int", nullable: false),
+                    Intelligence = table.Column<int>(type: "int", nullable: false),
+                    Wisdom = table.Column<int>(type: "int", nullable: false),
+                    Constitution = table.Column<int>(type: "int", nullable: false),
+                    Dexterity = table.Column<int>(type: "int", nullable: false),
+                    Charisma = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statistics", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Statistics_Characters_CharacterId",
+                        column: x => x.CharacterId,
+                        principalTable: "Characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Statistics_CharacterId",
+                table: "Statistics",
+                column: "CharacterId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "Statistics");
+
+            migrationBuilder.DropTable(
+                name: "Characters");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace inspirited
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Details { get; set; }
+        public Statistics Stats { get; set; }
+    }
+    public class Statistics
+    {
+        public int Id { get; set; }
+        public int CharacterId { get; set; }
+        public int Strength { get; set; }
+        public int Intelligence { get; set; }
+        public int Wisdom { get; set; }
+        public int Constitution { get; set; }
+        public int Dexterity { get; set; }
+        public int Charisma { get; set; }
+    }
+    public class Player
+    {
+        public int Id { get; set; }
+        public string PlayerName { get; set; }
+        public int CharacterId { get; set; }
+    }
+    public class ISDataContext : DbContext
+    {
+        string pomeloString = "server=localhost;database=InspiritedDb;user=root;password=passw0rd";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(pomeloString, ServerVersion.AutoDetect(pomeloString));
+        }
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<Statistics> Statistics { get; set; }
+        public DbSet<Player> Players { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to inspirited!");
+            LoadingCharacters();
+            LoadingStats();
+            Characters();
+        }
+
+        private static async void Characters()
+        {
+            Console.WriteLine("List of Characters");
+            using (var dbContext = new ISDataContext())
+            {
+                var characters = dbContext.Characters;
+                foreach (var character in characters)
+                {
+                    Console.WriteLine(character.Id+ "\t" +character.Type);
+                }
+            }
+            Console.WriteLine("Do you want to create your character? Y(es) or N(o)!");
+            char isCreate = Convert.ToChar(Console.ReadLine());
+            if (isCreate == 'Y' || isCreate =='y')
+            {
+                CreatePlayer();
+            }
+            else
+            {
+                Console.WriteLine("Enter player name:");
+                string playerName = Console.ReadLine();
+                using (var dbContext = new ISDataContext())
+                {
+                    var playerwithName = dbContext.Players.SingleOrDefault(c => c.PlayerName == playerName);
+                    if (playerwithName is null)
+                    {
+                        Console.WriteLine("Player Not Found");
+                    }
+                    IsStart(playerwithName.Id);
+                }
+            }
+        }
+
+        private async static void CreatePlayer()
+        {
+                Console.WriteLine("Enter Player Name:");
+                string playerName = Console.ReadLine();
+                Console.WriteLine("Enter selected Character ID:");
+                int charactedId = Convert.ToInt16(Console.ReadLine());
+                using (var dbContext = new ISDataContext())
+                {
+                    var playerwithSameName = dbContext.Players.FirstAsync(c => c.PlayerName == playerName);
+                    if (playerwithSameName is not null) return;
+                    var player = new Player()
+                    {
+                        PlayerName = playerName,
+                        CharacterId = charactedId
+                    };
+                    dbContext.Add(player);
+                    await dbContext.SaveChangesAsync();
+                    Console.WriteLine(player.PlayerName +" is created.");
+                    IsStart(player.Id);
+                }
+        }
+
+        private static void IsStart(int Id)
+        {
+                    Console.WriteLine("Do you want to start? Y(es) or N(o)!");
+                    char isStart = Convert.ToChar(Console.ReadLine());
+                    if (isStart == 'Y' || isStart == 'y')
+                    {
+                        GameStart(Id);
+                    }
+                    else
+                    {
+                        return;
+                    }
+        }
+
+        private static void GameStart(int Id)
+        {
+            LoadPlayer(Id);
+        }
+
+        private static void LoadPlayer(int Id)
+        {
+            using (var dbContext = new ISDataContext())
+            {
+                var playerwithId = dbContext.Players.Find(Id);
+                if (playerwithId is null) return;
+            }
+        }
+
+        private static void LoadingStats()
+        {
+            using (var dbContext = new ISDataContext())
+            {
+                if (dbContext.Statistics.Any())
+                {
+                    return;
+                }
+                var stat = new Statistics
+                {
+                    CharacterId = 1,
+                    Constitution = 25,
+                    Strength = 25,
+                    Wisdom = 25,
+                    Intelligence = 25,
+                    Dexterity = 25,
+                    Charisma = 25
+                };
+            }
+        }
+
+        private static async void LoadingCharacters()
+        {
+            using (var dbContext = new ISDataContext())
+            {
+                if (dbContext.Characters.Any())
+                {
+                    return;
+                }
+
+                var character = new Character
+                {
+                    Type = "Swordsman",
+                    Details = "A person with swords"
+                };
+                dbContext.Add(character);
+                await dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
